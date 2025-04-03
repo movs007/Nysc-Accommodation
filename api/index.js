52578d4619b60d489a159c22fe6f3097cb07c13b
movs007 <muves218@gmail.com>
2025-04-03 09:00:18 +0100

    Fix: Remove front-end static file serving

--- api/index.js
+++ api/index.js
@@ -41,11 +41,11 @@ app.use('/api/preference', preferenceRouter);
 app.use('/api/recommendation', recommendationRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -72,7 +72,7 @@ export default function Home() {
       </PopUp>
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+        <h1 className='text-black font-bold text-3xl lg:text-6xl'>
           Find your next <span className='' style={{color:"#202185",}}>perfect</span>
           <br />
           place with ease
